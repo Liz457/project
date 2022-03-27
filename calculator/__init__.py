8d78828f0db9df198651be1f7a61ffcd109c5ef1
Elizabeth <eliam9799@gmail.com>
2022-03-27 13:42:58 -0400

    calculator code added

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -5,16 +5,21 @@ class Calculator:
     """ This is the default result property"""
     result = 0
 
-    def add(self, value_1):
+    def add(self, value_1,value_2):
         """ This is the add method"""
-        self.result = self.result + value_1
+        self.result = value_1 + value_2
         return self.result
 
-    def subtract(self, value_1):
+    def subtract(self, value_1,value_2):
         """ This is the subtract method"""
-        self.result = self.result - value_1
+        self.result =value_1 -value_2
         return self.result
 
     def get_result(self):
         """ This is the get result method"""
         return self.result
+
+    def multiply(self, value_1,value_2):
+        """This is the multiply method"""
+        self.result =value_1* value_2
+        return  self.result
---
+++ calculator/operations/__init__.py
@@ -0,0 +1,31 @@
+""" These are the Operation Classes"""
+
+
+class Addition:
+    """This is the addition class"""
+
+    # this defines a static method that you can use without instantiating the class
+    @staticmethod
+    def add(value_1, value_2):
+        """ This is the add method"""
+        return value_1 + value_2
+
+
+class Subtraction:
+    """This is the subtraction class"""
+
+    @staticmethod
+    def subtract(value_1, value_2):
+        """This is the subtract method"""
+        return value_1 - value_2
+
+class Multiplication:
+    """This is the multiplication method"""
+
+    @staticmethod
+    def multiply(value_1, value_2):
+        """This is the add method"""
+        return value_1 * value_2
+
+
+
